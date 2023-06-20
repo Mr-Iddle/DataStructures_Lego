8a987acd83beff42f15d6405759d200b0e7ccaad
Corvin Wittmaack <corvin.wittmaack@student.nhlstenden.com>
2023-06-20 21:09:16 +0200

    Created Quicksort and legodata class

---
+++ LegoAlgorithm/LegoAlgorithm/LegoAlgorithm/LegoData.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegoAlgorithm
+{
+    internal class LegoData
+    {
+        private int _id;
+        private string _name;
+        private string _rgb;
+        private string _transparency;
+
+        public LegoData(int id, string name, string rgb, string transparency)
+        {
+            _id = id;
+            _name = name;
+            _rgb = rgb;
+            _transparency = transparency;
+        }
+
+        //getter and setter
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Rgb
+        {
+            get { return _rgb; }
+            set { _rgb = value; }
+        }
+
+        public string Transparency
+        {
+            get { return _transparency; }
+            set { _transparency = value; }
+        }
+
+        public override string ToString()
+        {
+            return "Id: " + _id + " Name: " + _name + " Rgb: " + _rgb + " Transparency: " + _transparency;
+        }
+    }
+}
---
+++ LegoAlgorithm/LegoAlgorithm/LegoAlgorithm/QuickSort.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegoAlgorithm
+{
+    internal class QuickSort
+    {
+        //create QuickSort method
+        public static void QuickSortMethodCustom<T>(T[] array, int left, int right) where T : IComparable
+        {
+            int i = left;
+            int j = right;
+            T pivot = array[(left + right) / 2];
+            while (i <= j)
+            {
+                while (array[i].CompareTo(pivot) < 0)
+                {
+                    i++;
+                }
+                while (array[j].CompareTo(pivot) > 0)
+                {
+                    j--;
+                }
+                if (i <= j)
+                {
+                    T tmp = array[i];
+                    array[i] = array[j];
+                    array[j] = tmp;
+                    i++;
+                    j--;
+                }
+            }
+            if (left < j)
+            {
+                QuickSortMethodCustom(array, left, j);
+            }
+            if (i < right)
+            {
+                QuickSortMethodCustom(array, i, right);
+            }
+        }
+    }
+}
