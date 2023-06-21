ceb969d4a88af6b46827bb8d724c704840d0b295
KrystianWiazowskiV2 <89790188+bouncychair@users.noreply.github.com>
2023-06-21 16:30:39 +0200

    BubbleSort and QuickSort implemented to the ArrayList

--- LegoAlgorithm/LegoAlgorithm/LegoAlgorithm/Program.cs
+++ LegoAlgorithm/LegoAlgorithm/LegoAlgorithm/Program.cs
@@ -14,9 +14,37 @@ namespace LegoAlgorithm
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            ChrisArrayList<int> myList = new ChrisArrayList<int>();
+
+            myList.Add(5);
+            myList.Add(8);
+            myList.Add(3);
+            myList.Add(2);
+            myList.Add(1);
+            myList.Add(4);
+            myList.Add(5);
+            myList.Add(6);
+            myList.Add(7);
+
+            myList.BubbleSort(0, 6);
+            Console.WriteLine(myList.ToString());
+            myList.Clear();
+            Console.WriteLine(myList.ToString());
+
+            myList.Add(5);
+            myList.Add(8);
+            myList.Add(3);
+            myList.Add(2);
+            myList.Add(1);
+            myList.Add(4);
+            myList.Add(5);
+            myList.Add(6);
+            myList.Add(7);
+            myList.QuickSort(0, 6);
+            Console.WriteLine(myList.ToString());
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
         }
     }
 }
